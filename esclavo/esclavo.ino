8a1b9ee466a652c22c83abe08457b25fbf2d0310
FreddyOjeda <freddy.ojeda@uptc.edu.co>
2024-09-04 00:44:32 -0500

    Restructracion de codigo

--- esclavo/esclavo.ino
+++ esclavo/esclavo.ino
@@ -2,6 +2,18 @@
 #include "Arduino.h"
 #include <Wire.h>
 #include "HT_SSD1306Wire.h"
+#include <string>
+
+#include "HT_DisplayUi.h"
+#include "heltec.h"
+
+#ifdef WIRELESS_STICK_V3
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_64_32, RST_OLED);  // addr , freq , i2c group , resolution , rst
+#else
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);  // addr , freq , i2c group , resolution , rst
+#endif
+
+DisplayUi ui(&display);
 
 #define RF_FREQUENCY 915000000  // Hz
 #define TX_OUTPUT_POWER 4       // dBm
@@ -20,7 +32,28 @@
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
 
-SSD1306Wire oledDisplay(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);  // addr , freq , i2c group , resolution , rst
+std::string ids = "...";
+int paquetes = 0;
+
+void frame1(ScreenDisplay *display, DisplayUiState *state, int16_t x, int16_t y) {
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawStringMaxWidth(0 + x, 10 + y, 128, "Sistema Iniciado");
+  display->drawStringMaxWidth(0 + x, y, 128, "Moderador #1");
+}
+
+void frame2(ScreenDisplay *display, DisplayUiState *state, int16_t x, int16_t y) {
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  std::string mensaje = "Paquetes Recibidos: " + std::to_string(paquetes);
+  display->drawStringMaxWidth(0 + x, 10 + y, 128, mensaje.c_str());
+  std::string mensaje2 = "IDs de Sensores: " + ids;
+  display->drawStringMaxWidth(0 + x, y, 128, mensaje2.c_str());
+}
+
+FrameCallback frames[] = { frame1, frame2 };
+int framesCant = 2;
+
 
 static RadioEvents_t RadioEvents;
 int16_t rssi, rxSize;
@@ -31,6 +64,10 @@ void onReceive(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
   rxSize = size;
   memcpy(rxpacket, payload, size);
   rxpacket[size] = '\0';
+
+  paquetes++;
+  ids = ids + " " + std::to_string(payload[0]);
+
   Serial.print("Paquete recibido de ID: ");
   Serial.print(payload[0]);
   Serial.print(" Contenido: ");
@@ -41,11 +78,12 @@ void onReceive(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
   Serial.print(rssi);
   Serial.print(" y SNR ");
   Serial.println(snr);
-  chanel_free = true;
 }
 
 void sendPacket() {
   Serial.println("Envio Correcto");
+  paquetes = 0;
+  ids = "";
   chanel_free = true;
   Radio.Rx(0);  // Preparar para recibir cualquier paquete de vuelta
 }
@@ -68,17 +106,29 @@ void setup() {
                     LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
                     0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
 
-  oledDisplay.init();
-  oledDisplay.setFont(ArialMT_Plain_10);
+  ui.setTargetFPS(60);
+  ui.setIndicatorPosition(BOTTOM);
+
+  ui.setIndicatorDirection(LEFT_RIGHT);
+
+  ui.setFrameAnimation(SLIDE_UP);
+
+  ui.setFrames(frames, framesCant);
+  ui.init();
 }
 
+static unsigned long lastSignalTime = 0;
 void loop() {
-  static unsigned long lastSignalTime = 0;
   unsigned long currentTime = millis();
 
-  oledDisplay.clear();
-  oledDisplay.drawString(0, 0, "LoRa Iniciado");
-  oledDisplay.drawString(0, 0, "Moderador #1");
+  int remainingTimeBudget = ui.update();
+
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    delay(remainingTimeBudget);
+  }
 
   // Envía la señal inicial cada minuto
   if (currentTime - lastSignalTime >= 60000) {
--- movil2/movil2.ino
+++ movil2/movil2.ino
@@ -1,5 +1,18 @@
 #include "LoRaWan_APP.h"
 #include "Arduino.h"
+#include <string>
+
+#include "HT_DisplayUi.h"
+#include "heltec.h"
+
+#ifdef WIRELESS_STICK_V3
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_64_32, RST_OLED);  // addr , freq , i2c group , resolution , rst
+#else
+static SSD1306Wire display(0x3c, 500000, SDA_OLED, SCL_OLED, GEOMETRY_128_64, RST_OLED);  // addr , freq , i2c group , resolution , rst
+#endif
+
+DisplayUi ui(&display);
+
 #define RF_FREQUENCY 915000000  // Hz
 #define TX_OUTPUT_POWER 5       // dBm
 
@@ -18,46 +31,61 @@
 char txpacket[BUFFER_SIZE];
 char rxpacket[BUFFER_SIZE];
 
-double txNumber;
-bool chanel_free = false;
-unsigned long delayTime;
+const long wait_time = ((MOVIL_ID * 1000) / 2) + 5000;  //este sera el tiempo de espera para iniciar el envio
+const long restart_time = 55000 - wait_time;            //este sera el tiempo que debe esperar el nodo para volver a escuchar
 
 static RadioEvents_t RadioEvents;
 
-void sendPacket() {
-  Serial.println("Paquete enviado correctamente");
-  chanel_free = false;  // Esperar la próxima señal de inicio
-  Radio.Sleep();        // Dormir la radio después de la transmisión
+std::string estado = "...";
+
+void frame1(ScreenDisplay *display, DisplayUiState *state, int16_t x, int16_t y) {
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  std::string mensaje = "Sensor #" + std::to_string(MOVIL_ID);
+  display->drawStringMaxWidth(0 + x, 10 + y, 128, mensaje.c_str());
 }
 
+void frame2(ScreenDisplay *display, DisplayUiState *state, int16_t x, int16_t y) {
+  display->setTextAlignment(TEXT_ALIGN_LEFT);
+  display->setFont(ArialMT_Plain_10);
+  display->drawStringMaxWidth(0 + x, y, 128, estado.c_str());
+}
+
+FrameCallback frames[] = { frame1, frame2 };
+int framesCant = 2;
+bool chanel_free = false;
+
+void sendPacket() {
+  Serial.println("Paquete enviado correctamente");
+  estado = "Envio Exitoso";
+}
+
+unsigned long rx_millis = 0;
 void onReceive(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr) {
   if (strncmp((char *)payload, "START", size) == 0) {
     Serial.println("Señal de inicio recibida.");
-    delayTime = (10000 / MOVIL_ID) + 10000;  // Calcula el tiempo de retardo
+    estado = "Señal Recibida";
+
+    rx_millis = millis();
     chanel_free = true;
-    Radio.Sleep();  // Dormir la radio durante 50 segundos
-    delay(50000);   // Esperar 50 segundos
-    Radio.Standby(); // Después de 50 segundos, vuelve a poner la radio en modo de espera
+
   } else {
     Serial.print("Paquete no reconocido: ");
+    estado = "cruce de informacion";
     for (uint16_t i = 1; i < size; i++) {
       Serial.print((char)payload[i]);
     }
-    Serial.println("_______________________________________");
-    Radio.Rx(0);  // Preparar para recibir la señal de inicio
+    Serial.println("");
   }
 }
 
 void timeOut() {
-  Radio.Sleep();
   Serial.println("Tiempo de espera para transmitir agotado");
 }
 
 void setup() {
   Serial.begin(115200);
   Mcu.begin(HELTEC_BOARD, SLOW_CLK_TPYE);
-
-  txNumber = 0;
 
   RadioEvents.TxDone = sendPacket;
   RadioEvents.TxTimeout = timeOut;
@@ -73,17 +101,47 @@ void setup() {
   Serial.println("LoRa inicializado.");
 
   Radio.Rx(0);  // Preparar para recibir la señal de inicio
-  Serial.println("Esperando señal de inicio...");
+  Serial.print("Esperando señal de inicio, tiempo de espera: ");
+  Serial.print(wait_time);
+  Serial.print(", Tiempo de reinicio: ");
+  Serial.println(restart_time);
+
+  estado = "Esperando...";
+
+  ui.setTargetFPS(60);
+  ui.setIndicatorPosition(BOTTOM);
+
+  ui.setIndicatorDirection(LEFT_RIGHT);
+
+  ui.setFrameAnimation(SLIDE_UP);
+
+  ui.setFrames(frames, framesCant);
+  ui.init();
 }
 
+
 void loop() {
-  if (chanel_free) {
-    delay(delayTime);  // Espera el tiempo específico basado en el ID
-    Serial.print("Enviando --- ");
+  unsigned long current_millis = millis();
+  if (current_millis - rx_millis >= wait_time && chanel_free == true) {
 
-    uint8_t payload[] = { MOVIL_ID, 'H', 'o', 'l', 'a', ' ', 'L', 'o', 'R', 'a' };  // Ejemplo con ID 1
+    chanel_free = false;
 
+    estado = "Preparando envio";
+    uint8_t payload[] = { MOVIL_ID, 'H', 'o', 'l', 'a', 'L', 'o', 'R', 'a' };  // Ejemplo con ID 1
     Radio.Send(payload, sizeof(payload));
   }
+  if (current_millis - rx_millis >= restart_time) {
+    Radio.Rx(0);
+  }
   Radio.IrqProcess();
+
+
+  int remainingTimeBudget = ui.update();
+
+  if (remainingTimeBudget > 0) {
+    // You can do some work here
+    // Don't do stuff if you are below your
+    // time budget.
+    delay(remainingTimeBudget);
+  }
 }
